c0d0f21a3981fb53fac516971195fcc85e748abe
rabbit-aaron <rabbit.aaron@gmail.com>
2024-04-06 23:11:22 +1100

    prepare for release

--- django_theme_css_loader/apps.py
+++ django_theme_css_loader/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DjangoThemeCssLoaderConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_theme_css_loader'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_theme_css_loader"
